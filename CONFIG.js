e4ad892dde14886559e8ae970dcce7d9a968915a
nguyen1310999 <nguyenln@thuanhai.com.vn>
2025-07-15 16:58:17 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Em bé ơi, anh có điều muốn nói. </br> Sau khi anh ra Huế nói chuyện với mẹ xong, em bé làm người yêu anh nha",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+    "Hãy trả lời một cách thật lòng nhé. Bé nhớ đồng ý á.",
+  buttonYes: "Bé đồng ý",
+  buttonNo: "Chắc chắn đồng ý",
+  titleModar: "Cảm ơn em bé nhiều.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Anh thương em bé nhiều lắm nha",
 };
